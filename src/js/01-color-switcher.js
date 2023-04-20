dafeb272126fa4a98d8a7b43f41265efa0440c6f
vanvalera <vmarton777@gmail.com>
2023-04-20 18:48:31 +0300

    complete1_task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const logger = () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+};
+let intervalId;
+btnStart.addEventListener('click', () => {
+  logger(0);
+  intervalId = setInterval(logger, 1000, 0);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
 
+btnStop.addEventListener('click', () => {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(intervalId);
+});
