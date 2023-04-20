01fa8ee9a38d228e7be86613ecf8460adc4ba893
vanvalera <115824927+vanvalera@users.noreply.github.com>
2023-04-20 16:02:49 +0000

    Deploying to gh-pages from @ vanvalera/goit-js-hw-09@dafeb272126fa4a98d8a7b43f41265efa0440c6f 🚀

---
+++ 01-color-switcher.aa82d511.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");var a,n=function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t};t.addEventListener("click",(function(){n(),a=setInterval(n,1e3,0),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.aa82d511.js.map
