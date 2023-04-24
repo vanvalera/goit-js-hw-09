f985d70244539ca42c2926b0f12239eaa2716783
vanvalera <vmarton777@gmail.com>
2023-04-24 23:06:58 +0300

    Complete_all

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const onSubmittingClick = event => {
+  event.preventDefault();
+  let { delay, step, amount } = event.target.elements;
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+  if (amount <= 0 || delay < 0 || step < 0) {
+    Notiflix.Notify.failure(` Please input correct values (>=0)`);
+    return;
+  }
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(` Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(` Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+  form.reset();
+};
+
+form.addEventListener('submit', onSubmittingClick);
